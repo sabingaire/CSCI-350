c2e236328f71535664ca8380d14ebe5f0b19f293
Sabin Gaire <sabin.gaire@bison.howard.edu>
2020-02-04 10:18:35 -0500

    Algorithm for first function

--- csci350.scm
+++ csci350.scm
@@ -4,6 +4,10 @@
 ;@02859054
 ;Structures of Programming language
 
+;Algorithm for question 1:
+;First we check for null list, if its null we return as it is
+;otherwise we need to add head to the tail of cdr
+
 
 
 
